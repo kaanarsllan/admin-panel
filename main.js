7ae1f6b4402344bc46aa5f0d360363f1d3c1ae0e
kaanarsllan <52761646+kaanarsllan@users.noreply.github.com>
2021-04-25 15:12:06 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+ const url = "https://reqres.in/api/users";
+   const table = document.getElementById("customerTable");
+   
+   
+   function getCustomerList() {
+
+
+ 
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      //console.log(data);
+
+    
+      for(user of data.data){
+          //console.log(user);
+
+        table.innerHTML += ` <tr>
+        <td><input type="text" class="form-control" id="" value="
+        ${user.first_name}"></td>
+        <td><input type="text" class="form-control" id="" value="${user.last_name}"></td>
+        <td><input type="text" class="form-control" id="" value="${user.email}"></td>
+        
+       </tr> `
+      }
+    })
+  );
+}
+
+getCustomerList();
+
+
+function refreshData(){
+    getCustomerList();
+}
+
+
+function addCustomer(){
+    let data={
+        first_name:document.getElementById("first_name").value ||"deger yok ",
+        last_name:document.getElementById("last_name").value ||"deger yok ",     
+       email:document.getElementById("email").value ||"deger yok "
+    }
+        
+        fetch(url,{
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(data)
+})
+.then(response=>response.json())
+.then(data=>{
+    console.log(data);
+  
+    table.innerHTML +=` <tr>
+    <td><input type="text" class="form-control" id="" value="
+    ${data.first_name}"></td>
+    <td><input type="text" class="form-control" id="" value="${data.last_name}"></td>
+    <td><input type="text" class="form-control" id="" value="${data.email}"></td>
+    
+   </tr> `
+
+
+
+})
+
+    
+}
+
+
+
+
